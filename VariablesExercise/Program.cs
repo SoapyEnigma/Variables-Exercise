4fbfcffdc31b305e670f7617472610b2acd2b9ee
SoapyEnigma <keithlekgolo@gmail.com>
2025-07-24 00:29:25 -0400

    Added Variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userName = "Ashton";
+            int userAge = 31;
+            char firstLetterFirstName = 'A';
+            bool isMale = true;
+            double fakeAge = -1000.0;
+            decimal dewey = 0.12345m;
+
+            Console.WriteLine($"My name is {userName}.\n" +
+                $"The first letter of my name is {firstLetterFirstName}.\n" +
+                $"I am {userAge} years old.\n" +
+                $"I tell people I am {fakeAge} years old.\n" +
+                $"{dewey} is just a random number.");
+
+            if (isMale)
+                Console.WriteLine("I am a male.\n");
+            else
+                Console.WriteLine("I am a female.\n");
         }
     }
 }
